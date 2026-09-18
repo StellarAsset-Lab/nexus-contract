74aed2327cf8f6b1efa62075cc6562e333c298da
Hollujay <locko.charles@gmail.com>
2026-09-18 15:10:43 +0100

    feat(order): add two-step admin transfer

--- contracts/order/src/lib.rs
+++ contracts/order/src/lib.rs
@@ -6,6 +6,7 @@ mod storage;
 mod types;
 
 use error::ContractError;
+use events::{AdminTransferCancelled, AdminTransferProposed, AdminTransferred};
 use soroban_sdk::{contract, contractimpl, Address, Env};
 use storage::DataKey;
 
@@ -49,6 +50,74 @@ impl Order {
 
     pub fn next_order_id(env: Env) -> u64 {
         env.storage().instance().get(&DataKey::NextOrderId).unwrap()
+    }
+
+    pub fn propose_admin(env: Env, new_admin: Address) -> Result<(), ContractError> {
+        Self::require_initialized(&env)?;
+
+        let admin = Self::admin(env.clone());
+        admin.require_auth();
+
+        if env.storage().instance().has(&DataKey::PendingAdmin) {
+            return Err(ContractError::PendingAdminAlreadySet);
+        }
+
+        env.storage()
+            .instance()
+            .set(&DataKey::PendingAdmin, &new_admin);
+
+        AdminTransferProposed {
+            new_admin: new_admin.clone(),
+        }
+        .publish(&env);
+
+        Ok(())
+    }
+
+    pub fn accept_admin(env: Env) -> Result<(), ContractError> {
+        Self::require_initialized(&env)?;
+
+        let pending_admin: Address = env
+            .storage()
+            .instance()
+            .get(&DataKey::PendingAdmin)
+            .ok_or(ContractError::NoPendingAdmin)?;
+
+        pending_admin.require_auth();
+
+        let previous_admin = Self::admin(env.clone());
+
+        env.storage()
+            .instance()
+            .set(&DataKey::Admin, &pending_admin);
+        env.storage().instance().remove(&DataKey::PendingAdmin);
+
+        AdminTransferred {
+            previous_admin,
+            new_admin: pending_admin,
+        }
+        .publish(&env);
+
+        Ok(())
+    }
+
+    pub fn cancel_admin_proposal(env: Env) -> Result<(), ContractError> {
+        Self::require_initialized(&env)?;
+
+        let admin = Self::admin(env.clone());
+        admin.require_auth();
+
+        let pending_admin: Address = env
+            .storage()
+            .instance()
+            .get(&DataKey::PendingAdmin)
+            .ok_or(ContractError::NoPendingAdmin)?;
+
+        env.storage().instance().remove(&DataKey::PendingAdmin);
+
+        AdminTransferCancelled { pending_admin }.publish(&env);
+
+        Ok(())
     }
 
     fn require_initialized(env: &Env) -> Result<(), ContractError> {
